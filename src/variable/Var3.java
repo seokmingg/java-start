84e55f1b5d1068ab03e443e2bcc87913c010eb52
seokmin <devhong0016@gmail.com>
2025-10-31 22:30:12 +0900

    변수

---
+++ src/variable/Var3.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        //변수 선언
+        int a ;
+        int b ;
+        int c,d;
+        //처음으로 값을 지정하는거를 초기화라고함
+        a=5;
+
+        //선언과 초기화를동시에
+        int e=3;
+
+        int f=4,g=5;
+
+
+    }
+
+}
+
---
+++ src/variable/Var4.java
@@ -0,0 +1,32 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a= 100;   //정수
+        double b=10.5;
+        boolean c= true;
+        boolean d= false;
+        char c1='a';
+//        char c2='bd'; // 안됨 char는 한긂자만
+        String e = "aㅋ f ㅋ f ㄴ ㄴ ㄴ ㄴ f ㄹ";
+
+        //정수
+        byte b1= 127; //-128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; //-2,147,483,648 ~ 2,147,483,647 (약 20억)
+//-9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+        long l = 9223372036854775807L;
+
+
+//실수
+        float f = 10.0f;
+        double d1= 10.0; //좀더범위가넓다
+
+
+        //정수는 int 실수는 double을 일반적으로 사용한다
+
+
+
+
+    }
+}
