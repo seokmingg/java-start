daeb0c65728e4d82569bf4e8a76a2dcaeb430fd2
seokmin <devhong0016@gmail.com>
2025-11-01 00:46:40 +0900

    연산자

---
+++ src/operator/Operator1.java
@@ -0,0 +1,21 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+
+        int a=5;
+        int b=2;
+        int sum = a+b;
+        System.out.println("a+b="+sum);
+
+//        double div=a/b;  //더블해도 무슨 2.0이나오네
+        double div1= (double) a /b;
+        int div=a/b;
+        System.out.println("a/b="+div);
+        System.out.println("a/b="+div1);
+
+
+
+
+    }
+}
---
+++ src/operator/Operator2.java
@@ -0,0 +1,19 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+        String result="hello "+"world";
+        System.out.println("result = " + result);
+
+
+        String s1="hello1";
+        String s2="world2";
+        String result1=s1+s2;
+        System.out.println("result1 = " + result1);
+
+        String result3="a+b="+10;
+        System.out.println("result3 = " + result3);
+
+
+    }
+}
