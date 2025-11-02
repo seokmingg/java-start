f871c1b908651c00886ff121109be1cdc99b724f
seokmin <devhong0016@gmail.com>
2025-11-03 00:10:37 +0900

    문자열비교

---
+++ src/operator/Comp1.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Comp1 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        System.out.println(b==a);
+
+        String str1="문자열1";
+        String str2="문자열1";
+
+
+        System.out.println(str1.equals(str2));
+        //
+
+    }
+}
