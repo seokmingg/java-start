5e8943c39bb2845161b3c82904439547e726c371
seokmin <devhong0016@gmail.com>
2025-11-04 00:07:59 +0900

    if문

---
+++ src/cond/If1.java
@@ -0,0 +1,13 @@
+package cond;
+
+public class If1 {
+    public static void main(String[] args) {
+        int age = 10;
+        if (18<=age) {
+            System.out.println("성인");
+        }else System.out.println("미성년자");
+
+
+
+    }
+}
