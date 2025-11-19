d12225b37790e40a87b00afa8ed3efb552a59e87
seokmin <devhong0016@gmail.com>
2025-11-19 23:11:44 +0900

    method

---
+++ src/method/Method1.java
@@ -0,0 +1,22 @@
+package method;
+
+public class Method1 {
+
+     public static void main(String[] args) {
+         int sum1 = add(3,5);
+
+         System.out.println("sum1 = " + sum1);
+
+         int sum2 = add(5,5);
+            System.out.println("sum2 = " + sum2);
+    }
+
+    public static int add(int a, int b) {
+
+
+
+         return  a + b;
+    }
+
+
+}
