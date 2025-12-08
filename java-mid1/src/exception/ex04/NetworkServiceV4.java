435a9e7fd830dd3122277aeaae9ef6a4aa03c420
seokmin <devhong0016@gmail.com>
2025-12-09 00:05:49 +0900

    런타임에러,언체크에러

---
+++ java-mid1/src/exception/ex04/NetworkServiceV4.java
@@ -0,0 +1,15 @@
+package exception.ex04;
+
+public class NetworkServiceV4 {
+    public void sendMessage(String data) {
+        String address = "https://example.com";
+        NetworkClientV4 client = new NetworkClientV4(address);
+        client.initError(data);
+        try {
+            client.connect();
+            client.send(data);
+        } finally {
+            client.disconnect();
+        }
+    }
+}
---
+++ java-mid1/src/exception/ex04/exception/NetworkClientExceptionV4.java
@@ -0,0 +1,9 @@
+package exception.ex04.exception;
+
+
+
+public class NetworkClientExceptionV4 extends RuntimeException {
+    public NetworkClientExceptionV4(String message) {
+        super(message);
+    }
+}
