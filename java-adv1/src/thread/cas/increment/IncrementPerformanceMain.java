1598c38cfaccca7586bd4861eb580da491663ce2
seokmin <devhong0016@gmail.com>
2026-04-19 02:10:42 +0900

    atomic test

---
+++ java-adv1/src/thread/cas/increment/IncrementPerformanceMain.java
@@ -0,0 +1,37 @@
+package thread.cas.increment;
+
+public class IncrementPerformanceMain {
+
+    public static final long COUNT=100_000_000;
+
+
+    public static void main(String[] args) {
+
+        test(new BasicInteger());
+        test(new SyncInteger());
+        test(new MyAtomicInteger());
+
+    }
+
+
+
+    private static void test(IncrementInteger incrementInteger){
+
+        long startMs=System.currentTimeMillis();
+
+        for (long i = 0; i < COUNT ; i++) {
+            incrementInteger.increment();
+        }
+
+
+        long endMs=System.currentTimeMillis();
+
+        System.out.println(incrementInteger.getClass().getSimpleName()+"걸린시간"+(endMs-startMs));
+
+
+
+    }
+
+
+
+}
