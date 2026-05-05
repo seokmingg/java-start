2d01a853c621e311259988851dea25a1700b03b0
seokmin <devhong0016@gmail.com>
2026-05-05 12:24:27 +0900

    lambda test1,2

---
+++ java-adv3/java-adv3/src/lambda/ex2/FilterExample.java
@@ -0,0 +1,29 @@
+package lambda.ex2;
+import java.util.ArrayList;
+import java.util.List;
+public class FilterExample {
+    // 고차 함수, 함수를 인자로 받아서 조건에 맞는 요소만 뽑아내는 filter
+    public static List<Integer> filter(List<Integer> list, MyPredicate predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (int val : list) {
+            if (predicate.test(val)) {
+                result.add(val);
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(-3, -2, -1, 1, 2, 3, 5);
+                System.out.println("원본 리스트: " + numbers);
+
+        List<Integer> minus = filter(numbers, val -> val < 0);
+
+        System.out.println("minus = " + minus);
+
+
+// 1. 음수(negative)만 뽑아내기
+// 코드 작성
+// 2. 짝수(even)만 뽑아내기
+// 코드 작성
+    }
+}
