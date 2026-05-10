9d5d6e955d5b8a15e0260b874b61a635b3586c61
seokmin <devhong0016@gmail.com>
2026-05-10 11:51:03 +0900

    lambda +

---
+++ java-adv3/java-adv3/src/lambda/lambda4/ConsumerMain.java
@@ -0,0 +1,20 @@
+package lambda.lambda4;
+
+import java.util.function.Consumer;
+public class ConsumerMain {
+    public static void main(String[] args) {
+// 익명 클래스
+        Consumer<String> consumer1 = new Consumer<>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        };
+        consumer1.accept("hello consumer");
+// 람다
+        Consumer<String> consumer2 = (s) -> System.out.println(s);
+        Consumer<String> consumer3 = System.out::println;
+        consumer2.accept("hello consumer");
+        consumer3.accept("hello consumer");
+    }
+}
