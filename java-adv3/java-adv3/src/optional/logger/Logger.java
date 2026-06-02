793e595aef6b59a19dff50be75dbacf71344992d
seokmin <devhong0016@gmail.com>
2026-06-02 09:05:29 +0900

    optional

---
+++ java-adv3/java-adv3/src/optional/logger/Logger.java
@@ -0,0 +1,19 @@
+package optional.logger;
+
+import java.util.function.Supplier;
+
+public class Logger {
+    private boolean isDebug = false;
+    public boolean isDebug() {
+        return isDebug;
+    }
+    public void setDebug(boolean debug) {
+        isDebug = debug;
+    }
+    // DEBUG로 설정한 경우만 출력 - 데이터를 받음
+    public void debug(Object message) {
+        if (isDebug) {
+            System.out.println("[DEBUG] " + message);
+        }
+    }
+}
